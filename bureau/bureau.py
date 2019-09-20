8a70a43988a4ff8f790bae3db6cb8b703f555b1b
Kallou0 <nzenzamk@gmail.com>
2019-09-20 08:37:48 +0200

    Adding bureau.py

--- bureau/bureau.py
+++ bureau/bureau.py
@@ -8,20 +8,18 @@ from . import app,db
 @app.route('/registerbureau', methods = ['GET', 'POST'])  
 def registerbureau():
     form = BureauForm()
-    if form.validate():
+    if form.validate_on_submit():
         bureau = Bureau(name = form.name.data,
-                              address = form.address.data,
-                              email = form.email.data,
-                              account_no = form.account_no.data,
-                              destination_bank = form.destination_bank.data,
-                              longitude = form.longitude.data,
-                              latitude = form.latitude.data,
-                              username = form.username.data,
-                              password_hash = generate_password_hash(form.password_hash.data))
-                              
-
+                        address = form.address.data,
+                        email = form.email.data,
+                        account_no = form.account_no.data,
+                        destination_bank = form.destination_bank.data,
+                        longitude = form.longitude.data,
+                        latitude = form.latitude.data,
+                        username = form.username.data,
+                        password_hash = generate_password_hash(form.password_hash.data))
         bureau.save_to_db()
-        flash('Logged in successfully.')
+        flash('you can login now')
         return redirect(url_for('login'))
     return render_template('/buyer.html', form=form)
 
@@ -36,4 +34,26 @@ def login():
         else:
             flash('access denied, make sure you are entering correct credentials')
             return redirect(url_for('login'))
-    return redirect(url_for('registerbureau'))
+    return redirect(url_for('login'))
+
+@app.route('/landing')
+def landing():
+    return render_template('landing/index.html')
+
+@app.route('/log')
+def log():
+    return render_template('/landing/login.html')
+
+@app.route('/about')
+def about():
+    return render_template('/landing/about.html')
+
+@app.route('/services')
+def services():
+    return render_template('/landing/services.html')
+
+@app.route('/register')
+def register():
+    return render_template('/landing/register.html')
+
+    

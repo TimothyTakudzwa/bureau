453ca9891af0f237f1a19c21dd7005ad81cfbef5
Phantamosgoria <tinotendamhishi@protonmail.com>
2019-09-20 15:16:10 +0200

    [Genesis commit. Models.py ] Created Requests Model

--- bureau/dashboard.py
+++ bureau/dashboard.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, url_for
+from flask import Flask, render_template, request, flash, url_for. session
 from functools import wraps
 from flask import Flask, render_template, request, flash, url_for, redirect
 from .forms import ClientForm

b4b7049c15be80b08667b50b939f6db486f3710f
Kallou0 <nzenzamk@gmail.com>
2019-09-20 16:58:20 +0200

    Adding registration.html

--- bureau/__init__.py
+++ bureau/__init__.py
@@ -1,10 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+#from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 app.config.from_object('config')
+
+#login_manager = LoginManager()
+#login_manager.session_protection = 'strong'
+#login_manager.login_view = 'main.login'
+
+
+
+#db.init_app(app)
+#login_manager.init_app(app)
+
 
 from . import views, models, app, db, bureau, client, rates, dashboard
